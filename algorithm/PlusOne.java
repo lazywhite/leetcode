1859edc82326060d37e647ca023f7d4b541e1e0b
white <346816483@qq.com>
2017-05-30 17:20:47 +0800

    update plus one to number presented by array

--- algorithm/PlusOne.java
+++ algorithm/PlusOne.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
  * You may assume the integer do not contain any leading zero, except the number 0 itself.
@@ -6,23 +8,38 @@
  * ie:  [1, 6, 9, 8] --> 1699;
  */
 public class PlusOne{
-    public static int plus(int[] digits) {
-        StringBuilder strb = new StringBuilder();
-        for(int i=0;i<digits.length;i++){
-            strb.append(digits[i]);
+    public static int[] plusOne(int[] digits) {
+        int carry = 1;
+        for(int i=digits.length-1;i>=0;i--){
+            int num = digits[i];
+            int rt = num + carry;
+            int left = rt % 10;
+            carry = rt / 10;
+            digits[i] = left;
+            if(carry == 1){
+                continue;
+            }else{
+                break;
+            }
         }
-        String s =  strb.toString();
-        Long l = Long.parseLong(s);
-        if(l+1 > Integer.MAX_VALUE){
-            return 0;
+        //循环结束， 要么中途已经没有进位了, 要么循环耗尽
+        if(carry == 1){//加到最后还有进位， 说明结果一定是1和n个0
+            int len = digits.length;
+            int[] newArray = new int[len + 1];
+            Arrays.fill(newArray, 0);
+            newArray[0] = 1;
+            return newArray;
+        }else{
+            return digits;
         }
-        return Integer.parseInt(s) + 1;
     }
 
     public static void main(String[] args){
         int[] digits = {1, 3, 4, 7, 9, 9,9,9,9,9,9, 9,9,9,9};
-        System.out.println(plus(digits));
-        int [] adigits = {1, 3, 7, 9};
-        System.out.println(plus(adigits));
+        System.out.println(Arrays.toString(plusOne(digits)));
+        int[] newdigits = {9,9,9,9,9,9,9, 9,9,9,9};
+        System.out.println(Arrays.toString(plusOne(newdigits)));
+        int[] arr = {9,9,9,9,1,9,9, 9,9,9,9};
+        System.out.println(Arrays.toString(plusOne(arr)));
     }
 }
